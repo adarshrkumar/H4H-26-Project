25136507791a2609fe9779ed4be2d0297571d7e5
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-02-23 14:57:36 -0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
+import webSpatial from "@webspatial/vite-plugin";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    vercel(),
+    react(),
+    webSpatial(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          XR_ENV: process.env.XR_ENV,
+        },
+      },
+    }),
+  ],
+});
